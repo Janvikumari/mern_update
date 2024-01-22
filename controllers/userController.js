83309287ccd541e48200e442206a427eabfe3082
Janvi kumari <86555518+Janvikumari@users.noreply.github.com>
2024-01-23 00:10:40 +0530

    commits

---
+++ controllers/userController.js
@@ -0,0 +1,93 @@
+const userModel = require('../models/userModel')
+const bcrypt = require('bcrypt')
+exports.registerController = async(req,res)=>{
+try{
+ const {username,email,password} = req.body
+ if(!username || !email || !password){
+    return res.status(400).send({
+        success:false,
+        message:'Please fill all the fields'
+    })
+ }
+ const existingUser = await userModel.findOne({email})
+ if(existingUser){
+    return res.status(401).send({
+    success:false,
+    message:'user already exists'
+    })
+ }
+ const hashedPassword = await bcrypt.hash(password,10)
+
+ const user = new userModel({username,email,password:hashedPassword})
+ await user.save()
+ return res.status(201).send({
+    success:true,
+    message:'New User Created',
+    user,
+ });
+} catch(error){
+    console.log(error)
+    return res.status(500).send({
+        message:'Error In Callback',
+        success:false,
+        error
+    })
+}
+}
+exports.getAllUsers = async(req,res)=>{
+    try{
+      const users = await userModel.find({});
+      return res.status(200).send({
+        userCount: users.length,
+        success:true,
+        message:"all users data",
+        users,
+      });
+    }catch(error){
+    console.log(error)
+    return res.status(500).send({
+        success:false,
+        message:'Error in getting users',
+        error
+    })
+    }
+
+};
+
+exports.loginController = async(req,res)=>{
+ try{
+ const {email,password} = req.body
+ if(!email || !password){
+    return res.status(401).send({
+        success:false,
+        message:'please provide email or password'
+    })
+ }
+ const user = await userModel.findOne({email})
+ if(!user){
+    return res.status(200).send({
+        success:false,
+        message:'email is not registered'
+    })
+ }
+ const isMatch = await bcrypt.compare(password, user.password)
+ if(!isMatch){
+    return res.status(401).send({
+        success:false,
+        message:'Invalid Password or username'
+    })
+ }
+ return res.status(200).send({
+    success:true,
+    message:'login successfully',
+    user
+ })
+ }catch(error){
+  console.log(error)
+  return res.status(500).send({
+    success: false,
+    message: "Server Error",
+    error
+  })
+ }
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const colors = require('colors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const connectDB = require("./config/connect");
+
+dotenv.config();
+const userRoutes = require('./routes/userRoutes')
+const blogRoutes = require('./routes/blogRoutes')
+connectDB();
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/blog",blogRoutes)
+
+app.get("/",(req,res) =>{
+    res.status(200).send({
+        message:"Node Server",
+    });
+});
+const PORT = process.env.PORT ||  5000;
+app.listen(PORT, () =>{
+    console.log(`Server is running on ${process.env.DEV_MODE} port no ${PORT}`.blue);
+});
